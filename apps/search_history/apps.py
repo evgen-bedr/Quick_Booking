624abf93f7efed3c4da4cb13e403de40a1f77a2e
Evgen <redrif48@gmail.com>
2024-07-10 10:20:03 +0200

    Created models: user_model and custom_user_model, applied migrations, and updated admin.py

--- apps/search_history/apps.py
+++ apps/search_history/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SearchHistoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'search_history'
+    name = 'apps.search_history'
---
+++ apps/users/models/user_model.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
+from apps.users.models.custom_user_model import CustomUserManager
+from apps.rentals.models.rental_model import Rental
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=18, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted = models.BooleanField(default=False)
+    is_landlord = models.BooleanField(default=False)
+    is_moderator = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='images/profile_pics/', null=True, blank=True)
+    favorites = models.ManyToManyField('rentals.Rental', related_name='favorited_by')
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
